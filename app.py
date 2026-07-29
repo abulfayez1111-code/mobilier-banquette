84f65925edf7bc3c255b2a93000660857c94373e
abulfayez1111-code <abulfayez1111@gmail.com>
2026-07-29 03:18:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,6 @@ def webhook():
         print(">>> WEBHOOK POST RECEIVED:", data)
         return "EVENT_RECEIVED", 200
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
